91057c5903ff40eaa029a6d1cb50cf99c37d789b
Apshara-125 <2312125@nec.edu.in>
2025-09-04 21:47:14 +0530

    fix vite base path for Jenkins

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/userContent/currency-converter/',   // 👈 super important
+  base: '/userContent/currency-converter/',
 })
